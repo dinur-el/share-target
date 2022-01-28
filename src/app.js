fbeaf61781b5032dfc2d1391ffcbdbc0998a2912
Dinura Wijayarathne <dinurawijayarathne@Dinuras-MacBook-Pro.local>
2022-01-27 23:23:52 -0500

    action url

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 alert("ssssssss");
 window.addEventListener('DOMContentLoaded', () => {
-    const parsedUrl = new URL(window.location);
-    // searchParams.get() will properly handle decoding the values.
-    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
-    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
-    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-    document.querySelector('h1').innerHTML = "sss";//parsedUrl.searchParams.get('url');
+    // const parsedUrl = new URL(window.location);
+    // // searchParams.get() will properly handle decoding the values.
+    // console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    // console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    // console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+    // document.querySelector('h1').innerHTML = "sss";//parsedUrl.searchParams.get('url');
   });
