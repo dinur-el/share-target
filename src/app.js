a6679443d9953a5fe6ac1c259633c0c5aae2ed24
Dinura Wijayarathne <dinurawijayarathne@Dinuras-MacBook-Pro.local>
2022-01-27 23:03:16 -0500

    action url

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
+if (window.location.href.includes('share-target')) {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
     console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
     console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
     console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-    document.querySelector('h1').innerHTML = "sss"//parsedUrl.searchParams.get('url');
-  });
+    document.querySelector('h1').innerHTML = "sss";//parsedUrl.searchParams.get('url');
+  }
